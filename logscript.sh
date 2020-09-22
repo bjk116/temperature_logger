1b4863d0b6b2d75bce0aa7785c937b128c4d93a3
root <root@ubuntu.home>
2020-09-22 18:53:13 +0000

    got rid of old logs at bottom, added comments

--- logscript.sh
+++ logscript.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 now="$(date)"
+# cat /sys/../temp is how you get the current temp of the raspberry pi
+# on ubuntu
 temp=$(cat /sys/class/thermal/thermal_zone0/temp)
 temp_f=$(printf '%s\n' "$temp/1000" | bc -l)
-echo temp_f $temp_f
 printf '%.3fC,%s\n' "$temp_f" "$now" >> /home/projects/temperature_logger/temp.log
-Tue Sep 22 13:54:31 UTC 2020
-Tue Sep 22 13:55:31 UTC 2020
