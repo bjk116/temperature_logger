4e378c96f240aea3909936e14e586c8399b4fbb2
Ubuntu <ubuntu@ubuntu.home>
2020-09-28 18:45:18 +0000

    New folder structure

--- shell/install.sh
+++ shell/install.sh
@@ -1,3 +1,4 @@
 sudo pip3 install pytz
 sudo pip3 install dateutil
 sudo pip3 install mysql-connector-python
+mysql_secure_installation
--- variables.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-var1=hello
-var2=everybody
-#
-echo "$var1 $var2"
